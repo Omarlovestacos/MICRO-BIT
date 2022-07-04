7c39cb9acb1d02c3157b72f045e05154e12917ff
Omar hijazi <107864121+Omarlovestacos@users.noreply.github.com>
2022-07-04 17:21:38 +0300

    Add files via upload

--- security system.py
+++ security system.py
@@ -1,24 +1,19 @@
 from microbit import *
-from ssd1306 import initialize, clear_oled
-from ssd1306_text import add_text
 from ultrasonic import Ultrasonic
 
 ultrasonic_sensor = Ultrasonic()
-
-initialize()
-clear_oled()
+pin0.set_analog_period(20)
 
 while True:
     distance_value = ultrasonic_sensor.measure_distance_cm()
-    #display.scroll(str(int(distance_value)))
+
     motion = pin1.read_digital()
     if motion == 1 and distance_value < 15:
-        add_text(0, 0, "motion detected")
-        pin0.write_digital(1)
+        pin1.write_digital(1)
         pin16.write_digital(1)
     else:
         pin16.write_digital(0)
-        add_text(0, 0, "no motion detected")
-        pin0.write_digital(0)
+        pin1.write_digital(0)
 
-
+while True:
+    pin0.write_analog(360)
